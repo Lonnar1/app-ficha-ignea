7fba1d3ddb5e98dd202bd5dd30b9bb514205c20d
Lonnar17 <lonnar321@gmail.com>
2026-04-19 00:54:43 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,11 @@
 self.addEventListener("install", (e) => {
-  console.log("Service Worker instalado");
+  console.log("Service Worker v2");
 });
 
 self.addEventListener("fetch", (e) => {
   // só pra ativar mesmo, sem cache por enquanto
+});
+
+self.addEventListener("activate", () => {
+  console.log("Nova versão ativa 🚀");
 });
