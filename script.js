1228e76e9df2f795dc8d137e5a4a76c9166a1dfa
Lonnar17 <lonnar321@gmail.com>
2026-06-06 15:01:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 // ===== SISTEMA DE PLANOS =====
 const EMAILS_VIP = [
   "fichaignea@gmail.com",
-  "l1onnar321@gmail.com"
+  "lonnar321@gmail.com",
+  "niviciusedney@gmail.com
 ];
 
 function getPlanoUsuario() {
